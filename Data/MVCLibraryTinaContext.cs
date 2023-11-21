f12b52f82676e4a79bd93877076a81f9e43dbb43
Tina Budimir <tibudimi@vsite.hr>
2023-11-21 14:33:32 +0100

    Add project files.

---
+++ Data/MVCLibraryTinaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCLibraryTina.Models;
+
+namespace MVCLibraryTina.Data
+{
+    public class MVCLibraryTinaContext : DbContext
+    {
+        public MVCLibraryTinaContext (DbContextOptions<MVCLibraryTinaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCLibraryTina.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCLibraryTina.Models
+{
+    public class Book
+    { 
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int ISBN { get; set; }
+    public string Author { get; set; }  
+}
+}
